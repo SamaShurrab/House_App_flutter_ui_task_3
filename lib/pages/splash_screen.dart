175efc0d5a413176b044c319077f4ed2703d08b4
Sama <smshorap@gmail.com>
2025-08-29 17:59:24 +0300

    Add SplashScreen widget and update main entry point; configure Gradle timeouts and update pubspec.lock dependencies

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget{
+  const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+   return SplashScreenState();
+  }
+
+}
+class SplashScreenState extends State<SplashScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }}
