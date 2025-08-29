c2d9c652ea94de05f9e68f356f3201f2527fce39
Sama <smshorap@gmail.com>
2025-08-29 18:36:50 +0300

    Refactor AppStrings class to add splash screen strings

--- lib/constants/string.dart
+++ lib/constants/string.dart
@@ -1,3 +1,6 @@
 class AppStrings{
-  
+// Splash screen string
+static const String welcomeStatement="Welcome to Smart House";
+static const String splashdescription="Find your dream home easily and quickly";
+static const String getStartedBtn="Get Started";
 }
